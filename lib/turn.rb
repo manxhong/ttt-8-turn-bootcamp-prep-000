f21fa8eaf0971d4abb573e3824ea8b049068fc59
Man Hong Lee <manhong323@gmail.com>
2017-10-29 00:25:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,8 +13,15 @@ def input_to_index(int)
 end
 
 def valid_move?(board, index)
-  
+
 end
 
 def position_taken?(board, index)
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  end
+
+  if (board[index] == "X" || board[index] == "O")
+    return true
+  end
 end
