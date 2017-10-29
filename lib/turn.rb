1b4c2fa945a1dd71b7e7ff6a4e3c634938fa5f79
Man Hong Lee <manhong323@gmail.com>
2017-10-29 00:22:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ def display_board(board)
   rows = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts rows
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts rows
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
