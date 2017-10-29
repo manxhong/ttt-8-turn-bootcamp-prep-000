6c8105d074f093558b108f00f2f7ab0f6339804a
Man Hong Lee <manhong323@gmail.com>
2017-10-29 00:26:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,11 @@ def input_to_index(int)
 end
 
 def valid_move?(board, index)
+  if (board[index].between?(0,8) && position_taken(board, index) ==false)
+    return true
+  end
 
+  return false
 end
 
 def position_taken?(board, index)
